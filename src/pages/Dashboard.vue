10db496b96044c7daa3ef06389411ebe13adebbc
s3ns3x <s3ns3xy@yahoo.com>
2020-10-26 18:38:38 +0800

    [ADD] - eslint config

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,59 +1,82 @@
 <template>
   <div>
     <div class="q-pa-md q-gutter-sm">
-    <q-card class="my-card">
-      <q-card-section>
-        {{ lorem }}
-        <div class="small">
-            <line-chart :chart-data="datacollection"></line-chart>
-            <button @click="fillData()">Randomize</button>
-        </div>
-      </q-card-section>
-    </q-card>
-    <q-card class="my-card">
-      <q-card-section>
-        {{ lorem }}
-        <div class="small">
-            <line-chart :chart-data="datacollection"></line-chart>
-            <button @click="fillData()">Randomize</button>
-        </div>
-      </q-card-section>
-    </q-card>
+      <q-card class="my-card">
+        <q-card-section>
+          {{ lorem }}
+          <div class="small">
+            <line-chart :chart-data="datacollection" />
+            <button @click="fillData()">
+              Randomize
+            </button>
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-card class="my-card">
+        <q-card-section>
+          {{ lorem }}
+          <div class="small">
+            <line-chart :chart-data="datacollection" />
+            <button @click="fillData()">
+              Randomize
+            </button>
+          </div>
+        </q-card-section>
+      </q-card>
 
-        <div class="row justify-evenly q-gutter-md q-pa-md">
-            <q-card flat bordered class="col text-center q-gutter-md q-pa-md" style="border-color: purple">
-                <q-avatar icon="money">
-                    
-                </q-avatar>
-                <q-separator light inset />
-                <div class="text-h5 text-weight-bold text-purple-8">36%</div>
-            </q-card>
+      <div class="row justify-evenly q-gutter-md q-pa-md">
+        <q-card
+          flat
+          bordered
+          class="col text-center q-gutter-md q-pa-md"
+          style="border-color: purple"
+        >
+          <q-avatar icon="money" />
+          <q-separator light inset />
+          <div class="text-h5 text-weight-bold text-purple-8">
+            36%
+          </div>
+        </q-card>
 
-            <q-card flat bordered class="col text-center q-gutter-md q-pa-md" style="border-color: purple">
-                <q-avatar icon="money">
-                    
-                </q-avatar>
-                <q-separator light inset />
-                <div class="text-h5 text-weight-bold text-purple-8">36%</div>
-            </q-card>
+        <q-card
+          flat
+          bordered
+          class="col text-center q-gutter-md q-pa-md"
+          style="border-color: purple"
+        >
+          <q-avatar icon="money" />
+          <q-separator light inset />
+          <div class="text-h5 text-weight-bold text-purple-8">
+            36%
+          </div>
+        </q-card>
 
-                <q-card flat bordered class="col text-center q-gutter-md q-pa-md" style="border-color: purple">
-                <q-avatar icon="money">
-                    
-                </q-avatar>
-                <q-separator light inset />
-                <div class="text-h5 text-weight-bold text-purple-8">36%</div>
-            </q-card>
-            <q-card flat bordered class="col text-center q-gutter-md q-pa-md" style="border-color: purple">
-                <q-avatar icon="money">
-                    
-                </q-avatar>
-                <q-separator light inset />
-                <div class="text-h5 text-weight-bold text-purple-8">36%</div>
-            </q-card>
-        </div>
+        <q-card
+          flat
+          bordered
+          class="col text-center q-gutter-md q-pa-md"
+          style="border-color: purple"
+        >
+          <q-avatar icon="money" />
+          <q-separator light inset />
+          <div class="text-h5 text-weight-bold text-purple-8">
+            36%
+          </div>
+        </q-card>
+        <q-card
+          flat
+          bordered
+          class="col text-center q-gutter-md q-pa-md"
+          style="border-color: purple"
+        >
+          <q-avatar icon="money" />
+          <q-separator light inset />
+          <div class="text-h5 text-weight-bold text-purple-8">
+            36%
+          </div>
+        </q-card>
+      </div>
     </div>
-
   </div>
 </template>
 
@@ -61,6 +84,13 @@
 import LineChart from '../boot/LineChart.js'
 
 export default {
+
+  components: {
+    LineChart
+    // Rolebox,
+    // Userbox,
+    // UserSessionbox,
+  },
   data() {
     return {
       datacollection: null,
@@ -69,38 +99,31 @@ export default {
       },
     }
   },
-  mounted () {
-  this.fillData()
-    },
-
-  components: {
-    LineChart
-    // Rolebox,
-    // Userbox,
-    // UserSessionbox,
+  mounted() {
+    this.fillData()
   },
 
-    methods: {
-    fillData () {
-    this.datacollection = {
+  methods: {
+    fillData() {
+      this.datacollection = {
         labels: [this.getRandomInt(), this.getRandomInt()],
         datasets: [
-        {
+          {
             label: 'Data One',
             backgroundColor: '#f87979',
             data: [this.getRandomInt(), this.getRandomInt()]
-        }, {
+          }, {
             label: 'Data One',
             backgroundColor: '#f87979',
             data: [this.getRandomInt(), this.getRandomInt()]
-        }
+          }
         ]
-    }
+      }
     },
-    getRandomInt () {
-    return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
     }
-    }
+  }
 }
 </script>
 
