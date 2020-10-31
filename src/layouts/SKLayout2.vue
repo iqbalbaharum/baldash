d300d05f5f932af99d83c1c016fcbb4ed937a80f
Dolpheyn <farissufyan99@gmail.com>
2020-10-31 17:36:52 +0800

    hotfix(sklayout2): lint

--- src/layouts/SKLayout2.vue
+++ src/layouts/SKLayout2.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="q-pa-md">
     <q-layout
-      view="lHh lpr lFf"      
+      view="lHh lpr lFf"
       style="height: 400px"
       class=" rounded-borders"
     >
@@ -15,7 +15,7 @@
             class="q-mr-sm"
           />
           <q-avatar>
-          <q-icon name="style" /> 
+            <q-icon name="style" />
           </q-avatar>
           <q-toolbar-title>SK</q-toolbar-title>
           <q-btn
@@ -25,41 +25,43 @@
             icon="whatshot"
           />
         </q-toolbar>
-      <q-tabs
-        v-model="tab"
-        inline-label
-        switch-indicator
-        active-color="black"
-        indicator-color="primary"
-        class="bg-grey-1 text-black shadow-2 "
-        align="justify"
-      >
-        <q-tab name="usermanagment" label="User Management"/>
-        <q-tab name="onlineleads"  label="Online Leads" />
-        <q-tab name="qualifiedleads"  label="Qualified Leads" />
-        <q-tab name="designproposal"  label="Design Proposal" />
-        <q-tab name="orderconfirmation"  label="Order Confirmation" />
-        <q-tab name="servicereport"  label="Service Report" />
-      </q-tabs>
-      <q-tabs
-        v-model="tab"
-        indicator-color="transparent"
-        active-color="black"
-        class="bg-white text-grey-9 shadow-0"
-        align="justify-left"
-      >
-        <q-tab name="contractor" icon="fas fa-user-plus"  label="Add User" />
-      </q-tabs>
+        <q-tabs
+          v-model="tab"
+          inline-label
+          switch-indicator
+          active-color="black"
+          indicator-color="primary"
+          class="bg-grey-1 text-black shadow-2 "
+          align="justify"
+        >
+          <q-tab name="usermanagment" label="User Management" />
+          <q-tab name="onlineleads" label="Online Leads" />
+          <q-tab name="qualifiedleads" label="Qualified Leads" />
+          <q-tab name="designproposal" label="Design Proposal" />
+          <q-tab name="orderconfirmation" label="Order Confirmation" />
+          <q-tab name="servicereport" label="Service Report" />
+        </q-tabs>
+        <q-tabs
+          v-model="tab"
+          indicator-color="transparent"
+          active-color="black"
+          class="bg-white text-grey-9 shadow-0"
+          align="justify-left"
+        >
+          <q-tab
+            name="contractor"
+            icon="fas fa-user-plus"
+            label="Add User"
+          />
+        </q-tabs>
       </q-header>
       <q-page-container>
         <q-page class="q-pa-md">
-        <router-view />
+          <router-view />
         </q-page>
       </q-page-container>
     </q-layout>
   </div>
-
-  
 </template>
 
 <script>
