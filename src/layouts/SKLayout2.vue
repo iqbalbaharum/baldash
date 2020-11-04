58888be405cf5e3617499b5263bc13b4ebaff7b7
naziiruddin95 <70147515+naziiruddin95@users.noreply.github.com>
2020-11-04 15:20:25 +0800

    Update Tabs

--- src/layouts/SKLayout2.vue
+++ src/layouts/SKLayout2.vue
@@ -25,6 +25,7 @@
             icon="whatshot"
           />
         </q-toolbar>
+<<<<<<< Updated upstream
       <q-tabs
         v-model="tab"
         inline-label
@@ -50,6 +51,246 @@
       >
         <q-tab name="contractor" icon="fas fa-user-plus"  label="Add User" />
       </q-tabs>
+=======
+        <q-tabs
+          v-model="tab"
+          inline-label
+          switch-indicator
+          active-color="black"
+          indicator-color="primary"
+          class="bg-grey-4 text-black shadow-2 "
+          align="justify"
+          active-bg-color="white"
+        >
+          <q-tab name="usermanagement" label="User Management" />
+          <q-tab name="onlineleads" label="Online Leads" />
+          <q-tab name="qualifiedleads" label="Qualified Leads" />
+          <q-tab name="designproposal" label="Design Proposal" />
+          <q-tab name="orderconfirmation" label="Order Confirmation" />
+          <q-tab name="servicereport" label="Service Report" />
+        </q-tabs>
+        <q-tab-panels
+          v-model="tab"
+          animated
+          swipeable
+          vertical
+          transition-prev="jump-up"
+          transition-next="jump-up"
+          class="text-black"
+        >
+          <q-tab-panel name="usermanagement" class="q-gutter-lg">
+            <div class="row">
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'adduser'"
+                active-class="my-menu-link"
+                @click="link = 'adduser'"
+              >
+                <q-btn
+                  icon="fas fa-user-plus"
+                  label="Add User"
+                  stack
+                  flat
+                />
+              </q-item>
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'edit'"
+                active-class="my-menu-link"
+                @click="link = 'edit'"
+              >
+                <q-btn
+                  icon="fas fa-edit"
+                  label="Edit"
+                  stack
+                  flat
+                />
+              </q-item>
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'delete'"
+                active-class="my-menu-link"
+                @click="link = 'delete'"
+              >
+                <q-btn
+                  icon="fas fa-trash-alt"
+                  label="Delete"
+                  stack
+                  flat
+                />
+              </q-item>
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'filter'"
+                active-class="my-menu-link"
+                @click="link = 'filter'"
+              >
+                <q-btn
+                  icon="fas fa-filter"
+                  label="Filter by"
+                  stack
+                  flat
+                />
+              </q-item>
+            </div>
+          </q-tab-panel>
+
+          <q-tab-panel name="onlineleads" class="q-gutter-lg">
+            <div class="row">
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'assign'"
+                active-class="my-menu-link"
+                @click="link = 'assign'"
+              >
+                <q-btn
+                  icon="fas fa-user-tag"
+                  label="Assign to"
+                  stack
+                  flat
+                />
+              </q-item>
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'disqualify'"
+                active-class="my-menu-link"
+                @click="link = 'disqualify'"
+              >
+                <q-btn
+                  icon="fas fa-user-minus"
+                  label="Disqualify"
+                  stack
+                  flat
+                />
+              </q-item>
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'delete'"
+                active-class="my-menu-link"
+                @click="link = 'delete'"
+              >
+                <q-btn
+                  icon="fas fa-trash-alt"
+                  label="Delete"
+                  stack
+                  flat
+                />
+              </q-item>
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'filter'"
+                active-class="my-menu-link"
+                @click="link = 'filter'"
+              >
+                <q-btn
+                  icon="fas fa-filter"
+                  label="Filter by"
+                  stack
+                  flat
+                />
+              </q-item>
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'addview'"
+                active-class="my-menu-link"
+                @click="link = 'addview'"
+              >
+                <q-btn
+                  icon="fas fa-binoculars"
+                  label="Add View"
+                  stack
+                  flat
+                />
+              </q-item>
+            </div>
+          </q-tab-panel>
+
+          <q-tab-panel name="qualifiedleads" class="q-gutter-lg">
+            <div class="row">
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'todesign'"
+                active-class="my-menu-link"
+                @click="link = 'todesign'"
+              >
+                <q-btn
+                  icon="fas fa-user-plus"
+                  label="To Design Proposal"
+                  stack
+                  flat
+                  style="max-width:100px"
+                />
+              </q-item>
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'disqualify'"
+                active-class="my-menu-link"
+                @click="link = 'disqualify'"
+              >
+                <q-btn
+                  icon="fas fa-edit"
+                  label="Disqualify"
+                  stack
+                  flat
+                />
+              </q-item>
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'delete'"
+                active-class="my-menu-link"
+                @click="link = 'delete'"
+              >
+                <q-btn
+                  icon="fas fa-trash-alt"
+                  label="Delete"
+                  stack
+                  flat
+                />
+              </q-item>
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'filter'"
+                active-class="my-menu-link"
+                @click="link = 'filter'"
+              >
+                <q-btn
+                  icon="fas fa-filter"
+                  label="Filter by"
+                  stack
+                  flat
+                />
+              </q-item>
+              <q-item
+                v-ripple
+                clickable
+                :active="link === 'addview'"
+                active-class="my-menu-link"
+                @click="link = 'addview'"
+              >
+                <q-btn
+                  icon="fas fa-binoculars"
+                  label="Add View"
+                  stack
+                  flat
+                />
+              </q-item>
+            </div>
+          </q-tab-panel>
+        </q-tab-panels>
+>>>>>>> Stashed changes
       </q-header>
       <q-page-container>
         <q-page class="q-pa-md">
@@ -68,7 +309,19 @@ export default {
     return {
       tab: 'mails',
       link: 'inbox',
+
     }
-  }
+  },
 }
+<<<<<<< Updated upstream
 </script>
+=======
+</script>
+
+<style scoped>
+.my-menu-link{
+  color: blue;
+  /* background: #F2C037 */
+}
+</style>
+>>>>>>> Stashed changes
