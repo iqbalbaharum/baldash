fec08b48b8bf93f562e98f88d12af2c3b20400c9
Dolpheyn <farissufyan99@gmail.com>
2020-12-04 04:00:30 +0800

    Add permissions getter

--- src/boot/menu.js
+++ src/boot/menu.js
@@ -3,6 +3,7 @@ import { hasPermission } from '../utils'
 // Generate menu list based on user permissions and menu configuration
 
 export default async({ store }) => {
+  await store.dispatch('GetInfo')
   // Safely deep clone the state without reference so we can modify it without mutating vuex data
   // https://www.freecodecamp.org/news/how-to-clone-an-array-in-javascript-1d3183468f6a/
   const menuConfig = JSON.parse(JSON.stringify(store.getters.menus))
