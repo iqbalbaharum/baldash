234159d7a45638324f47f5f4953e3409e2e6f903
Dolpheyn <farissufyan99@gmail.com>
2020-12-04 07:10:58 +0800

    Fix issue user has to refresh to see actual filtered menu

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,297 +1,299 @@
 import Permissions from '../../types/permissions'
 import { hasPermission } from '../../utils'
 
+const MENU_CONFIG = [
+  {
+    name: 'usermanagement',
+    permissions: [Permissions.MODULE_USER_MANAGEMENT],
+    label: 'SK Management',
+    request: {
+      get: 'GetAllUsers',
+      delete: 'DeleteUser'
+    },
+    default_datatab_title: 'Users',
+    config: {
+      selectiontype: 'multiple'
+    },
+    buttons: [
+      {
+        category: 'User',
+        tos: [
+          {
+            name: 'adduser',
+            permissions: [Permissions.USER_ADD],
+            icon: 'fas fa-user-plus',
+            label: 'Add User',
+            disabled: false
+          },
+          {
+            name: 'edituser',
+            permissions: [Permissions.USER_EDIT],
+            icon: 'fas fa-edit',
+            label: 'View/Edit',
+            disabled: false
+          },
+          {
+            name: 'delete',
+            permissions: [Permissions.USER_DELETE],
+            icon: 'fas fa-trash-alt',
+            label: 'Delete',
+            disabled: false
+          },
+        ]
+      },
+      {
+        category: 'Access',
+        tos: [
+          {
+            name: 'assignScope',
+            permissions: [Permissions.USER_ASSIGN_SCOPE],
+            icon: 'fas fa-user-tag',
+            label: 'Scope',
+            disabled: false
+          },
+          {
+            name: 'userassignrole',
+            permissions: [Permissions.USER_ASSIGN_ROLE],
+            icon: 'fas fa-user-tag',
+            label: 'Assign Role',
+            disabled: false
+          },
+        ]
+      }
+    ]
+  },
+  {
+    name: 'branch',
+    permissions: [Permissions.MODULE_BRANCH_MANAGEMENT],
+    label: 'Branch',
+    request: {
+      get: 'GetAllBranches',
+      delete: 'DeleteBranch'
+    },
+    default_datatab_title: 'Branches',
+    config: {
+      selectiontype: 'multiple'
+    },
+    buttons: [
+      {
+        category: 'Management',
+        tos: [
+          {
+            name: 'addbranch',
+            permissions: [Permissions.BRANCH_ADD],
+            icon: 'fas fa-user-plus',
+            label: 'Add Branch',
+            disabled: false
+          },
+          {
+            name: 'editbranch',
+            permissions: [Permissions.BRANCH_EDIT],
+            icon: 'fas fa-edit',
+            label: 'View/Edit',
+            disabled: false
+          },
+          {
+            name: 'delete',
+            permissions: [Permissions.BRANCH_DELETE],
+            icon: 'fas fa-trash-alt',
+            label: 'Delete',
+            disabled: false
+          },
+        ]
+      },
+      {
+        category: 'Auto Suggestion',
+        tos: [
+          {
+            name: 'rrbranch',
+            icon: 'fas fa-filter',
+            label: 'Arrange Branches',
+            disabled: false
+          },
+        ]
+      },
+    ]
+  },
+  {
+    name: 'item',
+    permissions: [Permissions.MODULE_ITEM_LIST],
+    label: 'Item List',
+    request: {
+      get: 'GetAllItems',
+      delete: 'DeleteBranch'
+    },
+    default_datatab_title: 'Item List',
+    config: {
+      selectiontype: 'multiple'
+    },
+    buttons: [
+      {
+        category: 'Template',
+        tos: [
+          {
+            name: 'importitemlist',
+            permissions: [Permissions.ITEM_LIST_IMPORT],
+            icon: 'fas fa-upload',
+            label: 'Import',
+            disabled: false,
+          },
+          {
+            name: 'downloadsample',
+            permissions: [Permissions.ITEM_LIST_DOWNLOAD],
+            icon: 'fas fa-download',
+            label: 'Download',
+            disabled: false
+          },
+          {
+            name: 'importitemhistory',
+            permissions: [Permissions.ITEM_LIST_VIEW_HISTORY],
+            icon: 'fas fa-history',
+            label: 'Import History',
+            disabled: false,
+          }
+        ]
+      },
+    ]
+  },
+  {
+    name: 'onlineleads',
+    permissions: [Permissions.MODULE_ONLINE_LEADS],
+    label: 'Online Leads',
+    request: {
+      get: 'GetOnlineLeads',
+      delete: 'DeleteLead'
+    },
+    default_datatab_title: 'Online Leads',
+    config: {
+      selectiontype: 'multiple'
+    },
+    buttons: [
+      {
+        category: 'Leads',
+        tos: [
+          {
+            name: 'qualifyOL',
+            permissions: [Permissions.ONLINE_LEADS_QUALIFY],
+            icon: 'fas fa-user-tag',
+            label: 'Assign To',
+            disabled: false
+          },
+          {
+            name: 'disqualifyOL',
+            permissions: [Permissions.ONLINE_LEADS_DISQUALIFY],
+            icon: 'far fa-star',
+            label: 'Disqualify',
+            disabled: false
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'qualifiedleads',
+    permissions: [Permissions.MODULE_QUALIFIED_LEADS],
+    label: 'Qualified Leads',
+    request: {
+      get: 'GetQualifiedLeads',
+      delete: 'DeleteLead'
+    },
+    default_datatab_title: `Qualified Leads`,
+    config: {
+      selectiontype: 'single'
+    },
+    buttons: [
+      {
+        category: 'Assignment',
+        tos: [
+          {
+            name: 'assigntodesign',
+            permissions: [Permissions.QUALIFIED_LEADS_TO_DESIGN],
+            icon: 'fas fa-check',
+            label: 'Select',
+            disabled: false,
+          },
+          {
+            name: 'qualifyQL',
+            permissions: [Permissions.QUALIFIED_LEADS_ASSIGN],
+            icon: 'fas fa-user-tag',
+            label: 'Assign To',
+            disabled: false
+          },
+        ]
+      },
+      {
+        category: 'Leads',
+        tos: [
+          {
+            name: 'walkinlead',
+            permissions: [Permissions.QUALIFIED_LEADS_ADD],
+            icon: 'fas fa-walking',
+            label: 'Walk In',
+            disabled: false
+          },
+          {
+            name: 'requalifyqlead',
+            permissions: [Permissions.QUALIFIED_LEADS_REQUALIFY],
+            icon: 'fas fa-star',
+            label: 'Requalify',
+            disabled: false
+          },
+          {
+            name: 'disqualifyOL',
+            permissions: [Permissions.QUALIFIED_LEADS_DISQUALIFY],
+            icon: 'far fa-star',
+            label: 'Disqualify',
+            disabled: false
+          },
+        ]
+      }
+    ]
+  },
+  {
+    name: 'designproposal',
+    permissions: [Permissions.MODULE_DESIGN_PROPOSAL],
+    label: 'Design Proposal',
+    default_data: 'GetAllLeads',
+    default_datatab_title: 'DesignCAD',
+    config: {
+      selectiontype: 'single'
+    },
+    buttons: [
+      {
+        category: 'DesignCAD',
+        tos: [
+          {
+            name: 'opendesigncad',
+            permissions: [Permissions.DESIGN_OPEN_EXTERNAL],
+            icon: 'fas fa-pencil-ruler',
+            label: 'Open External',
+            disabled: false,
+          }
+        ]
+      },
+    ]
+  },
+  {
+    name: 'orderconfirmation',
+    permissions: [Permissions.MODULE_ORDER_CONFIRMATION],
+    label: 'Order Confirmation',
+    config: {
+      selectiontype: 'single'
+    },
+  },
+  {
+    name: 'servicereport',
+    permissions: [Permissions.MODULE_SERVICE_REPORT],
+    label: 'Service Report',
+    config: {
+      selectiontype: 'single'
+    },
+  },
+]
+
 const app = {
   state: {
-    menus: [
-      {
-        name: 'usermanagement',
-        permissions: [Permissions.MODULE_USER_MANAGEMENT],
-        label: 'SK Management',
-        request: {
-          get: 'GetAllUsers',
-          delete: 'DeleteUser'
-        },
-        default_datatab_title: 'Users',
-        config: {
-          selectiontype: 'multiple'
-        },
-        buttons: [
-          {
-            category: 'User',
-            tos: [
-              {
-                name: 'adduser',
-                permissions: [Permissions.USER_ADD],
-                icon: 'fas fa-user-plus',
-                label: 'Add User',
-                disabled: false
-              },
-              {
-                name: 'edituser',
-                permissions: [Permissions.USER_EDIT],
-                icon: 'fas fa-edit',
-                label: 'View/Edit',
-                disabled: false
-              },
-              {
-                name: 'delete',
-                permissions: [Permissions.USER_DELETE],
-                icon: 'fas fa-trash-alt',
-                label: 'Delete',
-                disabled: false
-              },
-            ]
-          },
-          {
-            category: 'Access',
-            tos: [
-              {
-                name: 'assignScope',
-                permissions: [Permissions.USER_ASSIGN_SCOPE],
-                icon: 'fas fa-user-tag',
-                label: 'Scope',
-                disabled: false
-              },
-              {
-                name: 'userassignrole',
-                permissions: [Permissions.USER_ASSIGN_ROLE],
-                icon: 'fas fa-user-tag',
-                label: 'Assign Role',
-                disabled: false
-              },
-            ]
-          }
-        ]
-      },
-      {
-        name: 'branch',
-        permissions: [Permissions.MODULE_BRANCH_MANAGEMENT],
-        label: 'Branch',
-        request: {
-          get: 'GetAllBranches',
-          delete: 'DeleteBranch'
-        },
-        default_datatab_title: 'Branches',
-        config: {
-          selectiontype: 'multiple'
-        },
-        buttons: [
-          {
-            category: 'Management',
-            tos: [
-              {
-                name: 'addbranch',
-                permissions: [Permissions.BRANCH_ADD],
-                icon: 'fas fa-user-plus',
-                label: 'Add Branch',
-                disabled: false
-              },
-              {
-                name: 'editbranch',
-                permissions: [Permissions.BRANCH_EDIT],
-                icon: 'fas fa-edit',
-                label: 'View/Edit',
-                disabled: false
-              },
-              {
-                name: 'delete',
-                permissions: [Permissions.BRANCH_DELETE],
-                icon: 'fas fa-trash-alt',
-                label: 'Delete',
-                disabled: false
-              },
-            ]
-          },
-          {
-            category: 'Auto Suggestion',
-            tos: [
-              {
-                name: 'rrbranch',
-                icon: 'fas fa-filter',
-                label: 'Arrange Branches',
-                disabled: false
-              },
-            ]
-          },
-        ]
-      },
-      {
-        name: 'item',
-        permissions: [Permissions.MODULE_ITEM_LIST],
-        label: 'Item List',
-        request: {
-          get: 'GetAllItems',
-          delete: 'DeleteBranch'
-        },
-        default_datatab_title: 'Item List',
-        config: {
-          selectiontype: 'multiple'
-        },
-        buttons: [
-          {
-            category: 'Template',
-            tos: [
-              {
-                name: 'importitemlist',
-                permissions: [Permissions.ITEM_LIST_IMPORT],
-                icon: 'fas fa-upload',
-                label: 'Import',
-                disabled: false,
-              },
-              {
-                name: 'downloadsample',
-                permissions: [Permissions.ITEM_LIST_DOWNLOAD],
-                icon: 'fas fa-download',
-                label: 'Download',
-                disabled: false
-              },
-              {
-                name: 'importitemhistory',
-                permissions: [Permissions.ITEM_LIST_VIEW_HISTORY],
-                icon: 'fas fa-history',
-                label: 'Import History',
-                disabled: false,
-              }
-            ]
-          },
-        ]
-      },
-      {
-        name: 'onlineleads',
-        permissions: [Permissions.MODULE_ONLINE_LEADS],
-        label: 'Online Leads',
-        request: {
-          get: 'GetOnlineLeads',
-          delete: 'DeleteLead'
-        },
-        default_datatab_title: 'Online Leads',
-        config: {
-          selectiontype: 'multiple'
-        },
-        buttons: [
-          {
-            category: 'Leads',
-            tos: [
-              {
-                name: 'qualifyOL',
-                permissions: [Permissions.ONLINE_LEADS_QUALIFY],
-                icon: 'fas fa-user-tag',
-                label: 'Assign To',
-                disabled: false
-              },
-              {
-                name: 'disqualifyOL',
-                permissions: [Permissions.ONLINE_LEADS_DISQUALIFY],
-                icon: 'far fa-star',
-                label: 'Disqualify',
-                disabled: false
-              }
-            ]
-          }
-        ]
-      },
-      {
-        name: 'qualifiedleads',
-        permissions: [Permissions.MODULE_QUALIFIED_LEADS],
-        label: 'Qualified Leads',
-        request: {
-          get: 'GetQualifiedLeads',
-          delete: 'DeleteLead'
-        },
-        default_datatab_title: `Qualified Leads`,
-        config: {
-          selectiontype: 'single'
-        },
-        buttons: [
-          {
-            category: 'Assignment',
-            tos: [
-              {
-                name: 'assigntodesign',
-                permissions: [Permissions.QUALIFIED_LEADS_TO_DESIGN],
-                icon: 'fas fa-check',
-                label: 'Select',
-                disabled: false,
-              },
-              {
-                name: 'qualifyQL',
-                permissions: [Permissions.QUALIFIED_LEADS_ASSIGN],
-                icon: 'fas fa-user-tag',
-                label: 'Assign To',
-                disabled: false
-              },
-            ]
-          },
-          {
-            category: 'Leads',
-            tos: [
-              {
-                name: 'walkinlead',
-                permissions: [Permissions.QUALIFIED_LEADS_ADD],
-                icon: 'fas fa-walking',
-                label: 'Walk In',
-                disabled: false
-              },
-              {
-                name: 'requalifyqlead',
-                permissions: [Permissions.QUALIFIED_LEADS_REQUALIFY],
-                icon: 'fas fa-star',
-                label: 'Requalify',
-                disabled: false
-              },
-              {
-                name: 'disqualifyOL',
-                permissions: [Permissions.QUALIFIED_LEADS_DISQUALIFY],
-                icon: 'far fa-star',
-                label: 'Disqualify',
-                disabled: false
-              },
-            ]
-          }
-        ]
-      },
-      {
-        name: 'designproposal',
-        permissions: [Permissions.MODULE_DESIGN_PROPOSAL],
-        label: 'Design Proposal',
-        default_data: 'GetAllLeads',
-        default_datatab_title: 'DesignCAD',
-        config: {
-          selectiontype: 'single'
-        },
-        buttons: [
-          {
-            category: 'DesignCAD',
-            tos: [
-              {
-                name: 'opendesigncad',
-                permissions: [Permissions.DESIGN_OPEN_EXTERNAL],
-                icon: 'fas fa-pencil-ruler',
-                label: 'Open External',
-                disabled: false,
-              }
-            ]
-          },
-        ]
-      },
-      {
-        name: 'orderconfirmation',
-        permissions: [Permissions.MODULE_ORDER_CONFIRMATION],
-        label: 'Order Confirmation',
-        config: {
-          selectiontype: 'single'
-        },
-      },
-      {
-        name: 'servicereport',
-        permissions: [Permissions.MODULE_SERVICE_REPORT],
-        label: 'Service Report',
-        config: {
-          selectiontype: 'single'
-        },
-      },
-    ],
+    menus: MENU_CONFIG,
     activeMenu: 'usermanagement',
     tableSelection: [],
     dialog: '',
@@ -433,14 +435,17 @@ const app = {
     },
 
     // Generate menu list based on user permissions and menu configuration
-    async FilterMenus({ dispatch, state, rootState }) {
+    async FilterMenus({ dispatch, rootState }) {
       // Safely deep clone the state without reference so we can modify it without mutating vuex data
       // https://www.freecodecamp.org/news/how-to-clone-an-array-in-javascript-1d3183468f6a/
-      const menuConfig = JSON.parse(JSON.stringify(state.menus))
+      const menuConfig = JSON.parse(JSON.stringify(MENU_CONFIG))
       const userPermissions = rootState.user.permissions
 
       if (typeof userPermissions === 'undefined') return // TODO: Temporary, each user should have permissions
-      if (userPermissions.includes('SKAllPermissions')) return
+      if (userPermissions.includes('SKAllPermissions')) {
+        dispatch('SetMenu', menuConfig)
+        return
+      }
 
       const accessibleMenus = menuConfig.filter(menu => hasPermission(userPermissions, menu.permissions))
 
