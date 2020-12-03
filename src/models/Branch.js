0def1656e6711660fe547747515cb20915f1dcd3
Muhammad Naziiruddin Hamzah <muhd.naziiruddin@gmail.com>
2020-12-03 18:40:14 +0000

    Update table columns. Add createdat, updated add field

--- src/models/Branch.js
+++ src/models/Branch.js
@@ -22,7 +22,8 @@ export default class Branch extends Model {
       SSMNo: this.attr(0),
       GSTNo: this.attr(0),
       logo: this.attr(0),
-
+      createdAt: this.attr(''),
+      updatedAt: this.attr(''),
       leads: this.hasMany(Lead, 'branchId')
     }
   }
@@ -54,8 +55,8 @@ export default class Branch extends Model {
     { name: 'name', required: true, label: 'Name', align: 'left', field: 'name', sortable: true },
     { name: 'code', label: 'Code', align: 'left', field: 'code', sortable: true },
     { name: 'type', label: 'Type', align: 'left', field: 'type', sortable: true },
-    { name: 'telno', label: 'Branch', align: 'left', field: 'telno', sortable: true },
-    { name: 'faxno', label: 'Created At', align: 'left', field: 'faxno', sortable: true },
+    { name: 'telno', label: 'Tel. No', align: 'left', field: 'telno', sortable: true },
+    { name: 'faxno', label: 'Fax No.', align: 'left', field: 'faxno', sortable: true },
     { name: 'email', label: 'Email', align: 'left', field: 'email', sortable: true },
     { name: 'address1', label: 'Address Line 1', align: 'left', field: 'address1', sortable: true },
     { name: 'address2', label: 'Address Line 2', align: 'left', field: 'address2', sortable: true },
