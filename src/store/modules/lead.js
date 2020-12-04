233cbd251d82e8c119f87c74d58f00edf9f8c647
Dolpheyn <farissufyan99@gmail.com>
2020-12-04 08:50:18 +0800

    Refresh table after add qualified lead

--- src/store/modules/lead.js
+++ src/store/modules/lead.js
@@ -205,7 +205,7 @@ const lead = {
           .then(res => {
             Lead.insert({ data: res.data })
             dispatch('UpdateTab', {
-              name: 'Online Leads',
+              name: 'Qualified Leads',
               columns: Lead.columns,
               key: Lead.primaryKey,
               data: Lead.query().withAll().get()
