3628a26c8d6ec11806e3be240a597595a97898a2
Dolpheyn <farissufyan99@gmail.com>
2020-12-08 22:30:38 +0800

    Add dependency: Sentry

--- src/boot/plugins.js
+++ src/boot/plugins.js
@@ -1,8 +1,19 @@
 import * as VueApexCharts from 'vue-apexcharts'
+import Vue from 'vue'
+import * as Sentry from '@sentry/browser'
+import { Integrations } from '@sentry/tracing'
 
 // "async" is optional;
 // more info on params: https://quasar.dev/quasar-cli/cli-documentation/boot-files#Anatomy-of-a-boot-file
-export default async({ Vue }) => {
+export default async() => {
   Vue.component('apexchart', VueApexCharts)
+
+  Sentry.init({
+    Vue,
+    integrations: [
+      new Integrations.BrowserTracing(),
+    ],
+    tracesSampleRate: 1.0,
+  })
 }
 
