4a09ea7090bb82d5fa89672638c99171e5325b9a
Dolpheyn <farissufyan99@gmail.com>
2020-12-11 17:37:34 +0800

    Task 23

--- src/components/Dialogs/SkEditUserDialog.vue
+++ src/components/Dialogs/SkEditUserDialog.vue
@@ -2,7 +2,8 @@
   <modal-dialog
     ref="dialog"
     name="edituser"
-    @close-dialog="reset"
+    @close-dialog="onHideDialog"
+    @show-dialog="onShowDialog"
   >
     <q-card style="width:1800px">
       <div>
@@ -33,60 +34,92 @@
                 User detail
               </div>
 
-              <div class="col">
-                <q-input
-                  v-model="form.name"
-                  outlined
-                  label="Fullname"
-                />
-              </div>
-              <div class="row ">
-                <q-input
-                  v-model="sccodemodel"
-                  class="col"
-                  outlined
-                  label="SC Code"
-                  @blur="onSCCodeCheck"
-                />
-                <q-input
-                  v-model="form.mobile"
-                  class="col q-pl-xs"
-                  outlined
-                  label="Telephone No."
-                />
-              </div>
-              <div class="col">
-                <q-input
-                  v-model="form.email"
-                  outlined
-                  label="Email"
-                />
-              </div>
-              <div class="col">
-                <q-select
-                  v-model="form.branchId"
-                  outlined
-                  :options="branches"
-                  label="Branch"
-                  emit-value
-                  map-options
-                  stack-label
-                />
-              </div>
-              <div class="text-negative">
-                {{ errormessage }}
-              </div>
+              <q-form ref="myForm" @submit="onUpdateUser">
+                <div class="col">
+                  <q-input
+                    v-model="form.name"
+                    outlined
+                    label="Fullname"
+                    :rules="[textRules]"
+                  />
+                </div>
+                <div class="row">
+                  <q-input
+                    v-model="form.sccode"
+                    class="col"
+                    debounce="500"
+                    outlined
+                    label="SC Code"
+                    :rules="[textRules, onSCCodeCheck]"
+                    :error="!!error.sccode"
+                  >
+                    <q-tooltip v-if="error.sccode">
+                      {{ error.sccode }}
+                    </q-tooltip>
+                  </q-input>
+
+                  <q-input
+                    v-model="form.mobile"
+                    class="col q-pl-xs"
+                    mask="### - #########"
+                    outlined
+                    unmasked-value
+                    label="Telephone No."
+                    :rules="[phoneNoRules, onMobileCheck]"
+                    :error="!!error.mobile"
+                  >
+                    <q-tooltip v-if="error.mobile">
+                      {{ error.mobile }}
+                    </q-tooltip>
+                  </q-input>
+                </div>
+
+                <div class="col">
+                  <q-input
+                    v-model="form.email"
+                    outlined
+                    label="Email"
+                    :rules="[emailRules || 'Wrong email format.']"
+                  />
+                </div>
+
+                <div class="col">
+                  <q-select
+                    v-model="form.branchId"
+                    outlined
+                    :options="branches"
+                    label="Branch"
+                    emit-value
+                    map-options
+                    stack-label
+                  />
+                </div>
+              </q-form>
+
+              <!-- Action button -->
               <div align="right">
                 <q-btn
                   v-close-popup
                   flat
                   color="primary"
+                  class="q-mx-sm"
                   label="Cancel"
                 />
                 <q-btn
+                  color="grey"
+                  label="Reset Form"
+                  class="q-mx-sm"
+                  @click="resetToUser"
+                >
+                  <q-tooltip>
+                    Reset form to user's original attributes.
+                  </q-tooltip>
+                </q-btn>
+                <q-btn
                   color="primary"
                   label="Update"
-                  :disabled="errormessage.length > 0"
+                  class="q-mx-sm"
+                  :disabled="!!error.name || !!error.sccode"
                   @click="onUpdateUser"
                 />
               </div>
@@ -99,23 +132,40 @@
 </template>
 
 <script>
-import User from '../../models/User'
 import Branch from '../../models/Branch'
 import Profile from '../../models/Profile'
 import { mapGetters } from 'vuex'
 import ModalDialog from './../ModalDialog'
+import validationMixin from '../../mixins/validationMixin.js'
 
 export default {
 
   components: {
     ModalDialog
   },
+
+  mixins: [
+    validationMixin,
+  ],
+
   data() {
     return {
-      userprofile: '',
       selectedUserId: '',
-      form: {},
-      errormessage: '',
+      user: {},
+      form: {
+        uuid: '',
+        name: '',
+        sccode: '',
+        mobile: '',
+        email: '',
+        branchId: '',
+      },
+      error: {
+        name: '',
+        sccode: '',
+        mobile: '',
+      },
+      selections: [],
     }
   },
 
@@ -133,57 +183,39 @@ export default {
       })
       return opts
     },
-    selections() {
-      const selections = this.$store.getters.tableSelection
-      if (!selections.length) return []
-      if (!(selections[0] instanceof User)) return []
-
-      const opts = selections.map((selection) => {
-        const container = []
-        container.label = selection.name
-        container.value = selection.uuid
-        return container
-      })
-
-      return opts
-    },
-    profile() {
-      const profile = Profile.query().where('userId', this.selectedUserId).first()
-      this.reassignProfile(profile)
-      return profile
-    },
-    sccodemodel: {
-      get() {
-        return this.form.sccode ? this.form.sccode : this.profile.sccode
-      },
-      set(value) {
-        this.form.sccode = value
-      }
-    },
-
-    getSelectedId() {
-      this.selections.find(async selection => {
-        if (selection.value === this.$store.getters.tableSelection[0].uuid) {
-          return selection.value
-        }
-      })
-      return ''
-    }
   },
 
   watch: {
     async selectedUserId(newValue, oldValue) {
+      if (!newValue) return
+
       const foundSelection = this.tableSelection.find((selection) => selection.uuid === newValue)
-      if (this.selectedUserId) {
-        await this.$store.dispatch('GetUserProfile', this.selectedUserId)
+      this.user = { ...foundSelection } // For reset to original
+
+      try {
+        await this.$store.dispatch('GetUserProfile', newValue)
+      } catch (e) {
+        this.$notify('error', 'Error getting user profile')
       }
-      this.form = { ...foundSelection }
-      this.errormessage = ''
+
+      const profile = Profile.query().where('userId', this.selectedUserId).first()
+      this.user.sccode = profile?.sccode
+
+      this.form = {
+        uuid: foundSelection.uuid,
+        name: foundSelection.name,
+        sccode: profile?.sccode,
+        mobile: foundSelection.mobile,
+        email: foundSelection.email,
+        branchId: foundSelection.branchId,
+      }
+
+      this.error = {
+        name: '',
+        sccode: '',
+        mobile: '',
+      }
     },
-    selections(newValue, oldValue) {
-      if (!newValue.length) return
-      this.selectedUserId = newValue[0].value
-    }
   },
 
   async created() {
@@ -191,16 +223,41 @@ export default {
   },
 
   methods: {
-    reset() {
-      this.selectedUserId = ''
-      this.form = {}
-    },
-    reassignProfile(profile) {
-      this.form.sccode = profile.sccode
-    },
-    async onUpdateUser() {
-      this.onSCCodeCheck()
+    onShowDialog() {
+      if (!this.$store.getters.tableSelection.length) return
 
+      this.selections = this.$store.getters.tableSelection
+      this.selections = this.selections.map(selection => {
+        const container = {}
+        container.label = selection.name
+        container.value = selection.uuid
+        return container
+      })
+      this.selectedUserId = this.selections[0].value
+    },
+
+    onHideDialog() {
+      this.selectedUserId = ''
+      this.form = {
+        uuid: '',
+        name: '',
+        sccode: '',
+        mobile: '',
+        email: '',
+        branchId: '',
+      }
+    },
+
+    resetToUser() {
+      this.form = { ...this.user }
+      this.error = {
+        name: '',
+        sccode: '',
+        mobile: '',
+      }
+    },
+
+    async onUpdateUser() {
       try {
         await this.$store.dispatch('UpdateUser', this.form)
         if (this.selections.length === 1) {
@@ -212,15 +269,33 @@ export default {
         this.$notify('error', message)
       }
     },
+
+    async onMobileCheck() {
+      this.error.mobile = ''
+      if (this.form.mobile === this.user.mobile) return true
+
+      await this.$store.dispatch('CheckMobileExist', this.form.mobile)
+        .then(exists => {
+          if (exists) {
+            this.error.mobile = 'Mobile already exist'
+          }
+        })
+
+      return !this.error.mobile
+    },
+
     async onSCCodeCheck() {
+      this.error.sccode = ''
+      if (this.form.sccode === this.user.sccode) return true
+
       await this.$store.dispatch('CheckSCCodeExist', this.form.sccode)
         .then(exists => {
           if (exists) {
-            this.errormessage = 'SCCode existed'
-          } else {
-            this.errormessage = ''
+            this.error.sccode = 'SCCode already exist'
           }
         })
+
+      return !this.error.sccode
     }
   }
 }
